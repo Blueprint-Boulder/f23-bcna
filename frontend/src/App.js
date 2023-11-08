66a82f590724913ba50896f0ae15087b152056e8
jacobef <jacobef2@gmail.com>
2023-11-08 16:45:06 -0700

    basic flask/react setup, no database

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/api').then(response => {
+      setMessage(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
