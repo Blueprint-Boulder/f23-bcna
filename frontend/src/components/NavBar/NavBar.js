e9ca44dae25a4322fbea80c9d9f8b11c68da2eb8
ohlsengrace <ohlsengrace@gmail.com>
2023-11-11 13:01:29 -0700

    Add styling to footer and navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,18 +1,24 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export const NavBar = () => {
     return (
         <div className="navbar">
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/wildlife">Wildlife</Link></li>
-                    <li><Link to="/checklists">Checklists</Link></li>
-                    <li><Link to="/resources">Resources</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
-                    <li><Link to="/api">API Test</Link></li>
+                <div className="flex flex-col items-center bg-white p-6">
+                {/* Logo Image */}
+                <div className="logo">
+                    <img src="https://via.placeholder.com/150" alt="Logo" />
+                </div>
+                <ul className="flex items-center space-x-4">
+                    <li><NavLink to="/" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Home</NavLink></li>
+                    <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-light-blue" : ""}>About</NavLink></li>
+                    <li><NavLink to="/wildlife" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Wildlife</NavLink></li>
+                    <li><NavLink to="/checklists" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Checklists</NavLink></li>
+                    <li><NavLink to="/resources" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Resources</NavLink></li>
+                    <li><NavLink to="/contact" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Contact</NavLink></li>
+                    <li><NavLink to="/api" className={({ isActive }) => isActive ? "text-light-blue" : ""}>API Test</NavLink></li>
                 </ul>
+                </div>
             </nav>
         </div>
     )
