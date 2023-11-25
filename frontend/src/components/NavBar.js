615fbac6f40fe54bb96b69e41c87f000324600a5
ohlsengrace <ohlsengrace@gmail.com>
2023-11-25 10:42:50 -0700

    changes to components

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,11 +11,7 @@ export const NavBar = () => {
                 </div>
                 <ul className="flex items-center space-x-4">
                     <li><NavLink to="/" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Home</NavLink></li>
-                    <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-light-blue" : ""}>About</NavLink></li>
                     <li><NavLink to="/wildlife" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Wildlife</NavLink></li>
-                    <li><NavLink to="/checklists" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Checklists</NavLink></li>
-                    <li><NavLink to="/resources" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Resources</NavLink></li>
-                    <li><NavLink to="/contact" className={({ isActive }) => isActive ? "text-light-blue" : ""}>Contact</NavLink></li>
                     <li><NavLink to="/api" className={({ isActive }) => isActive ? "text-light-blue" : ""}>API</NavLink></li>
                 </ul>
                 </div>
