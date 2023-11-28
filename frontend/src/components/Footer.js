1bef0d326ecea4ad2a3c8e18fb4f73f3191f73a2
Freddy <frederick.linn@colorado.edu>
2023-11-28 14:48:16 -0700

    remove fixed positioning from footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="bg-black text-white p-4 fixed bottom-0 left-0 w-full">
+        <footer className="bg-black text-white p-4">
         <p className="text-center">
           Copyright © 2023 Blueprint Boulder
         </p>
