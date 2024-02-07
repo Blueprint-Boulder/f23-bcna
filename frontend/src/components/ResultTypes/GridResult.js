cc918b0f06dbf399fc73ab8163612f0713853f94
ohlsengrace <ohlsengrace@gmail.com>
2024-02-07 16:12:23 -0700

    Alter Card, Grid, List

--- frontend/src/components/ResultTypes/GridResult.js
+++ frontend/src/components/ResultTypes/GridResult.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom"
+
 const GridResult = ({ data }) => {
     return (
         <tr className="grid-result">
-            <td>{data.name}</td>
+            <td><Link to={`/wildlife/${data.name}`}>{data.name}</Link></td>
             <td>{data.subcategory}</td>
         </tr>
     )
