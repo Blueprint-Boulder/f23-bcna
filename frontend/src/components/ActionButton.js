b26fc7843b2c21722e6e1f4024fd09498d29aaf9
Freddy <frederick.linn@colorado.edu>
2024-03-30 01:14:48 -0600

    remove button borders by default

--- frontend/src/components/ActionButton.js
+++ frontend/src/components/ActionButton.js
@@ -8,17 +8,17 @@ export const ActionButton = ({ onClick, size, color, children, noFocus, disabled
 
   switch(color){
     case("red"):
-      styles = `text-${size} bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 active:bg-red-700 
+      styles = `text-${size} bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 active:bg-red-700 border-none
                 focus:outline-none focus:ring focus:border-light-blue-dark flex gap-2 disabled:bg-gray-700`;
     break;
 
     case("green"):
-      styles = `text-${size} bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-600 active:bg-green-700
+      styles = `text-${size} bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-600 active:bg-green-700 border-none
                 focus:outline-none focus:ring focus:border-light-blue-dark flex gap-2 disabled:bg-gray-700`
     break;
 
     default:
-      styles = `text-${size} bg-light-blue text-white py-2 px-4 rounded-md hover:bg-blue-500 active:bg-blue-600
+      styles = `text-${size} bg-light-blue text-white py-2 px-4 rounded-md hover:bg-blue-500 active:bg-blue-600 border-none
                 focus:outline-none focus:ring focus:border-light-blue-dark flex gap-2 disabled:bg-gray-200`
     break;
   }
