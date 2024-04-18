540ffc108fb1083dc7a0e8fbc50ec471b6e98264
Freddy <frederick.linn@colorado.edu>
2024-04-18 15:38:42 -0600

    in the middle of updating API call format

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -4,7 +4,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'http://127.0.0.1:5000'
 
 const api = axios.create({
   baseURL: BASE_URL,
