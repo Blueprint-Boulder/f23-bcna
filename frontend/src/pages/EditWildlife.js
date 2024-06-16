2116239dc81908b48ef14a842a63d15ababa3539
Freddy <frederick.linn@colorado.edu>
2024-06-15 18:39:03 -0600

    prevent footer from blocking wildlifeDetails + editWildlife

--- frontend/src/pages/EditWildlife.js
+++ frontend/src/pages/EditWildlife.js
@@ -81,7 +81,7 @@ export default function EditWildlife() {
     return(
         <div className="bg-[url('https://images.squarespace-cdn.com/content/v1/5373ca62e4b0875c414542a1/1405111543624-681EMTDC5LLPE19MEUXH/image-asset.jpeg')] bg-cover w-screen h-[120vh]">
             <div className="h-8"></div>
-            <div className="bg-neutral-50 rounded-lg w-11/12 lg:w-3/5 mx-auto shadow-lg">
+            <div className="bg-neutral-50 rounded-lg w-11/12 lg:w-3/5 mx-auto shadow-lg overflow-auto">
                 <h1 className="text-3xl font-bold mb-8 pt-4 text-center">Edit Wildlife</h1>
                 <form onSubmit={handleSubmit} className="w-11/12 flex flex-col items-center lg:items-start mx-auto">
                     <div className="w-full lg:w-3/5">
@@ -123,8 +123,8 @@ export default function EditWildlife() {
                         )}
                     </div>
                     <div className="self-center flex gap-8 my-4 items-center">
-                            <ActionButton color="red" size="lg">Cancel</ActionButton>
-                            <ActionButton size="lg">Confirm</ActionButton>
+                            <ActionButton color="red" size="lg" onClick={() => window.location.href = window.location.pathname}>Cancel</ActionButton>
+                            <ActionButton size="lg" type="submit">Confirm</ActionButton>
                     </div>
                 </form>
             </div>
--- frontend/src/pages/WildlifeDetails.js
+++ frontend/src/pages/WildlifeDetails.js
@@ -57,14 +57,14 @@ export default function WildlifeDetails() {
     }, [wildlifeId]); // Re-run the effect when wildlifeId changes
 
     return (
-        <div className="bg-[url('https://images.squarespace-cdn.com/content/v1/5373ca62e4b0875c414542a1/1405111543624-681EMTDC5LLPE19MEUXH/image-asset.jpeg')] w-screen h-[120vh]">
+        <div className="bg-[url('https://images.squarespace-cdn.com/content/v1/5373ca62e4b0875c414542a1/1405111543624-681EMTDC5LLPE19MEUXH/image-asset.jpeg')] bg-cover w-screen h-[120vh]">
             <div className="h-8"></div>
-            <div className="bg-neutral-50 rounded-lg w-11/12 mx-auto shadow-lg">
+            <div className="bg-neutral-50 rounded-lg w-11/12 mx-auto shadow-lg overflow-auto h-[90%]">
                 {wildlife && (
                     <>
                         <h2 className="text-center text-2xl md:text-3xl pt-4 font-bold text-green-900 mb-8">{wildlife.name}</h2>
-                        <div className="md:flex md:justify-around pb-10">
-                            <div className="pl-10 md:pl-0 md:w-2/5">
+                        <div className="md:flex md:justify-around md:flex-wrap pb-10">
+                            <div className="pl-10 w-11/12 lg:pl-0 lg:w-2/5">
                                 {Object.entries(filteredData).map(([key, value]) => (
                                     <div key={key} className="mb-5">
                                         <h3 className="inline text-xl font-bold">{key}: </h3>
@@ -73,12 +73,12 @@ export default function WildlifeDetails() {
                                 ))}
                             </div>
                             <div className="flex flex-col items-center">
-                            {images.length >= 1 && <img src={`http://127.0.0.1:5000/api/get-image/${highlight}`} alt="" className="mb-5 w-80 h-64 object-cover" />}
+                            {images.length >= 1 && <img src={`http://127.0.0.1:5000/api/get-image/${highlight}`} alt="" className="mb-5 min-w-80 h-64 object-cover" />}
                             <div className="flex gap-2 p-4 flex-wrap justify-center">
                             {images.length >= 2 && (
                                 images.map(image => (
                                     <button key={wildlife[image]} onClick={() => setHighlight(wildlife[image])}>
-                                                <img draggable="false" className={"object-cover w-32 h-16 md:w-36 md:h-16 rounded-md " + (highlight === wildlife[image] ? " border-sky-500 border-2" : "hover:border-sky-300 hover:border-2")}
+                                                <img draggable="false" className={"object-cover min-w-32 h-16 md:w-36 md:h-16 rounded-md " + (highlight === wildlife[image] ? " border-sky-500 border-2" : "hover:border-sky-300 hover:border-2")}
                                                     src={`http://127.0.0.1:5000/api/get-image/${wildlife[image]}`} alt="" />
                                     </button>
                                 ))
