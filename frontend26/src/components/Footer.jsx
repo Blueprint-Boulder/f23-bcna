85b64324e24917c22322c98bd43ad591a76a3f74
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2026-03-30 13:40:36 -0600

    sand color

--- frontend26/src/components/Footer.jsx
+++ frontend26/src/components/Footer.jsx
@@ -5,37 +5,37 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer class="text-taupe-700 p-5 bg-taupe-100 border-t border-taupe-200">
+    <footer class="text-sand-700 p-5 bg-sand-100 border-t border-sand-200">
       <div class="rounded p-5 max-w-[1000px] mx-auto">
         <div class="flex justify-between">
           <div>
             <img src="/bcna_wordmark.png" height="100" />
             <h3>BOULDER COUNTY NATURE ASSOCIATION</h3>
-            <p class="text-taupe-400">P.O. Box 493, Boulder, CO 80306</p>
+            <p class="text-sand-400">P.O. Box 493, Boulder, CO 80306</p>
           </div>
           <div class="flex flex-col">
-            <h5 class="text-taupe-300 font-['Montserrat',sans-serif] m-0 mb-2.5">PAGES</h5>
-            <a href="" class="text-taupe-700 no-underline text-lg font-['Playfair_Display',serif]">
+            <h5 class="text-sand-300 font-['Montserrat',sans-serif] m-0 mb-2.5 font-bold">PAGES</h5>
+            <a href="" class="text-sand-700 no-underline text-lg font-['Playfair_Display',serif]">
               fireflies
             </a>
-            <a href="" class="text-taupe-700 no-underline text-lg font-['Playfair_Display',serif]">
+            <a href="" class="text-sand-700 no-underline text-lg font-['Playfair_Display',serif]">
               wildlife
             </a>
-            <a href="" class="text-taupe-700 no-underline text-lg font-['Playfair_Display',serif]">
+            <a href="" class="text-sand-700 no-underline text-lg font-['Playfair_Display',serif]">
               glossary
             </a>
-            <a href="" class="text-taupe-700 no-underline text-lg font-['Playfair_Display',serif]">
+            <a href="" class="text-sand-700 no-underline text-lg font-['Playfair_Display',serif]">
               admin
             </a>
           </div>
           <div class="flex flex-col">
-            <h5 class="text-taupe-300 font-['Montserrat',sans-serif] m-0 mb-2.5">RELATED WEBSITES</h5>
-            <a href="" class="text-taupe-700 no-underline text-lg font-['Playfair_Display',serif]">
+            <h5 class="text-sand-300 font-['Montserrat',sans-serif] m-0 mb-2.5 font-bold">RELATED WEBSITES</h5>
+            <a href="" class="text-sand-700 no-underline text-lg font-['Playfair_Display',serif]">
               main website
             </a>
           </div>
         </div>
-        <hr class="border-none border-t border-taupe-200" />
+        <hr class="border-none border-t border-sand-200" />
         <div class="flex justify-between">
           <p>
             Made by{" "}
@@ -44,8 +44,8 @@ export const Footer = () => {
             </a>
           </p>
           <div>
-            {/* <a href="" class="text-taupe-700"><Icon name="mdi:facebook" size={40} color="inherit" /></a> */}
-            {/* <a href="" class="text-taupe-700"><Icon name="mdi:instagram" size={40} color="inherit" /></a> */}
+            {/* <a href="" class="text-sand-700"><Icon name="mdi:facebook" size={40} color="inherit" /></a> */}
+            {/* <a href="" class="text-sand-700"><Icon name="mdi:instagram" size={40} color="inherit" /></a> */}
           </div>
         </div>
       </div>
